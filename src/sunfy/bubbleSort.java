a22df2afaede88b4276967c25fd091e376a10c79
sunfeiyang <sfy5941@163.com>
2019-03-06 12:46:32 +0800

    add bubblesort

---
+++ src/sunfy/bubbleSort.java
@@ -0,0 +1,25 @@
+package sunfy;
+
+public class bubbleSort {
+
+    public static void main(String[] args){
+        int[] arr = {1,3,65,34,56,8};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("原始数据---"+arr[i]);
+        }
+        for(int i=0;i< arr.length;i++){
+            for(int j=0;j < arr.length - i - 1;j++){
+                // 倒序
+                if(arr[j+1] > arr[j]){
+                    int a = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = a;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("排序后的数据为："+ arr[i]);
+        }
+
+    }
+}
